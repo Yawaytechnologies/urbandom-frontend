38ab44e47a8716c3aa24a45ad588d1e56c1ebeb8
santhosh-yaway <santhoshkumar@yawaytech.com>
2025-07-10 13:02:20 +0530

    sidebar import corrected

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
-import Sidebar from '../components/common/Sidebar';
+import Sidebar from '../components/common/SideBar';
 
 const MainLayout = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
