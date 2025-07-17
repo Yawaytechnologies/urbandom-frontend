e8f3ce80f70da790b4826feeb03ae1b7e71414cd
lokeshvd1 <lokesh@yawaytech.com>
2025-07-17 18:08:37 +0530

    changes made in axios

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,10 @@ import LandingPage from './pages/LandingPage';
 import UserActivity from './pages/UserActivity';
 import RentPage from './pages/RentPage';
 import BuyPage from './pages/BuyPage';
-<<<<<<< HEAD
 import PGPage from './pages/PGPage';
 
-=======
 import PropertyTypeSelector from './components/PropertyForm/PropertyType'; 
 // import SearchResults from './pages/SearchResults';
->>>>>>> c959e5f8a4e38de209c825d1e5084b59edcf428a
 
 const App = () => {
   return (
@@ -21,11 +18,8 @@ const App = () => {
         <Route path="/" element={<BuyPage/>} />
         <Route path="/buy" element={<BuyPage />} />
         <Route path="/rent" element={<RentPage />} />
-<<<<<<< HEAD
         <Route path="/pg" element={<PGPage />} />
-=======
         <Route path="/propertyform" element={<PropertyTypeSelector />} />
->>>>>>> c959e5f8a4e38de209c825d1e5084b59edcf428a
         {/* <Route path="/search" element={<SearchResults />} /> */}
         <Route path="/activity" element={<UserActivity />} />
       </Routes>
