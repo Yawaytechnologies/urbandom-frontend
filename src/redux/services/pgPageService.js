b075da632300543cd479ef322e6c2893b74fc130
sathish-dev <sathish@yawaytech.com>
2025-07-28 11:54:30 +0530

    Yaway-17

--- src/redux/services/pgPageService.js
+++ src/redux/services/pgPageService.js
@@ -7,7 +7,7 @@ const pgPageService = {
   // Fetch All Properties
   getNeighbourhoodPgs: async () => {
     try {
-      const response = await axios.get(`${API_URL}property/lookingTo/rent`);  // Get all properties (no ID needed)
+      const response = await axios.get(`${API_URL}property/lookingTo/pg-co-living`);  // Get all properties (no ID needed)
       return response.data; // Return the list of properties
     } catch {
       throw new Error('Failed to fetch properties');
